56460c743b7cb22965feec902388867061659ae9
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2026-01-28 22:24:54 -0600

    feat: tabs in HomePage

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -1,11 +1,15 @@
 import CustomJomboTron from "@/components/custom/CustomJomboTron"
 import HeroStats from "@/heroes/components/HeroStats"
 import HeroGrid from "@/heroes/components/HeroGrid"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { useState } from "react"
 
 
-  
+ type Tabs = "All" | "Favorites" | "Villains" | "Heroes"; 
 
 function HomePage() {
+
+  const [activeTab,setActiveTab] = useState<Tabs>("All");
   return (
     <>
         {/* Header */}
@@ -13,10 +17,53 @@ function HomePage() {
 
 
         {/* HeroStats */}
-        <HeroStats/>
+        <HeroStats/>  
 
-        {/* Hero Cards Grid */}
-        <HeroGrid/>
+        <Tabs value={activeTab} className="mb-8">
+          <TabsList>
+            <TabsTrigger 
+            onClick={()=> setActiveTab("All")} 
+            value="All">Todos</TabsTrigger>
+            <TabsTrigger 
+            onClick={()=> setActiveTab("Favorites")} 
+            value="Favorites">Favoritos
+            </TabsTrigger>
+
+            <TabsTrigger 
+            onClick={()=> setActiveTab("Villains")} 
+            value="Villains">Villanos
+            </TabsTrigger> 
+
+            <TabsTrigger 
+            onClick={()=> setActiveTab("Heroes")} 
+            value="Heroes">Heroes
+            </TabsTrigger> 
+
+          </TabsList>
+
+
+          <TabsContent value="All">
+            <h1>Todos los heroes</h1>
+            <HeroGrid/>
+          </TabsContent>
+
+          <TabsContent value="Favorites">
+            <h1>Favoritos</h1>
+            <HeroGrid/>
+          </TabsContent>
+
+          <TabsContent value="Villains">
+            <h1>Villanos</h1>
+            <HeroGrid/>
+          </TabsContent>
+          
+          <TabsContent value="Heroes">
+            <h1>Heroes</h1>
+            <HeroGrid/>
+          </TabsContent>
+
+
+      </Tabs>
     </>
   )
 }
